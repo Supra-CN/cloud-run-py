73b226b5cc3a0ac9d0d0cce9f3b3cdffb9093e16
wangjia20 <wangjia20@baidu.com>
2025-02-26 16:05:40 +0800

    清理无用代码

--- main.py
+++ main.py
@@ -89,7 +89,5 @@ def launch():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     launch()
-else:
-    launch()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
